f508a36811200076779dd7591154e7310ea05e71
birb <60079016+svxf@users.noreply.github.com>
2024-07-29 17:13:46 -0700

    Update giveTokens.js | format broke for some reason

--- commands/mudae/giveTokens.js
+++ commands/mudae/giveTokens.js
@@ -20,35 +20,35 @@ module.exports = {
 				.setDescription('amount of tokens to give')
 				.setRequired(true)),
 	async execute(interaction) {
-        const amount = interaction.options.getInteger('amount');
-
+	        const amount = interaction.options.getInteger('amount');
+	
 		// checks if user has an account
 		const userOption = interaction.options.getString('user');
 		const userDatabase = path.join(__dirname, 'data/users.csv');
 		const users = await readCsv(userDatabase);
-
+	
 		let userUpdated = false;
 		let targetedUsername = "";
-        const updatedUsers = users.map(user => {
-            if (user.userId === userOption) {
-                let balance = parseInt(user.balance, 10);
-                if (isNaN(balance)) balance = 0;
-
-                balance += amount;
-                user.balance = balance.toString();
-				targetedUsername = user.username;
-                userUpdated = true;
-            }
-            return user;
-        });
-
-        if (userUpdated) {
-            const updatedCsv = stringify(updatedUsers, { header: true });
-            fs.writeFileSync(userDatabase, updatedCsv);
-
-            await interaction.reply(`gave ${targetedUsername} ${amount} credits! their balance is now ${updatedUsers.find(user => user.userId === userOption).balance}`);
-        } else {
-            await interaction.reply(`${targetedUsername} does not have an existing account.`);
-        }
+	        const updatedUsers = users.map(user => {
+	            if (user.userId === userOption) {
+	                let balance = parseInt(user.balance, 10);
+	                if (isNaN(balance)) balance = 0;
+	
+	                balance += amount;
+	                user.balance = balance.toString();
+					targetedUsername = user.username;
+	                userUpdated = true;
+	            }
+	            return user;
+	        });
+	
+	        if (userUpdated) {
+	            const updatedCsv = stringify(updatedUsers, { header: true });
+	            fs.writeFileSync(userDatabase, updatedCsv);
+	
+	            await interaction.reply(`gave ${targetedUsername} ${amount} credits! their balance is now ${updatedUsers.find(user => user.userId === userOption).balance}`);
+	        } else {
+	            await interaction.reply(`${targetedUsername} does not have an existing account.`);
+	        }
 	},
 };
